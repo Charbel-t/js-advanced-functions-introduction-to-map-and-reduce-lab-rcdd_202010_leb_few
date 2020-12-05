3cb8b72ba6c6b4f2836f4178ed47446d643db61f
Charbel t <charbel2000.52@gmail.com>
2020-12-05 11:52:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+function mapToNegativize(array){
+  for(let i=0;i<array.length;i++){
+    array[i]= -array[i];
+  }
+  return array;
+}
+
+function mapToNoChange(arr){
+  let r=[];
+  for(let i=0;i<arr.length;i++)
+    r.push(arr[i]);
+  return r;  
+}
+
+function mapToDouble(arr){
+  let r=[];
+  for(let i=0;i<arr.length;i++)
+    r.push(arr[i]*2);
+  return r;  
+}
+
+function mapToSquare(arr){
+  let r=[];
+  for(let i=0;i<arr.length;i++)
+    r.push(arr[i]**2);
+  return r;  
+}
+
+function reduceToTotal()
